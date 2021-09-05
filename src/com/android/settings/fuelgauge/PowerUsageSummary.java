184a9a26c70ce564273cd965fef4be84961be088
sairam1411 <majgaonkars3@gmail.com>
2021-09-05 14:28:47 +0530

    Smart Cutoff: Make it optional
    * Allows a device maintainer to show/hide the Smart Cutoff pref in Power Settings
    * Disabled by default
    
    Based upon https://github.com/ForkLineageOS/android_packages_apps_Settings/commit/911d8a2931463a22abf32f2e05c75c6ed3aab785
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -241,6 +241,11 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
             getPreferenceScreen().removePreference(mSmartCharging);
         }
+        // Check availability of Smart Cutoff
+        Preference mSmartCutoff = (Preference) findPreference("smart_cutoff_key");
+        if (!getResources().getBoolean(R.bool.config_supportSmartCutoff)) {
+            getPreferenceScreen().removePreference(mSmartCutoff);
+       }
     }
 
     @Override
